8c05f678beaa74b892a6edec001deff476edbf66
Heindale <heindale@yandex.ru>
2024-01-17 16:24:49 +0500

    update IHomewrokService

--- HomewOurK/Interfaces/IHomeworkService.cs
+++ HomewOurK/Interfaces/IHomeworkService.cs
@@ -5,9 +5,11 @@ namespace HomewOurK.WebAPI.Services.Interfaces
 	public interface IHomeworkService
 	{
 		List<Homeworks> GetHomeworksByGroupId(int groupId);
-		Homeworks GetHomeworkById(int homeworkId, int subjectId, int groupId);
+		List<Homeworks> GetHomeworksBySubjectId(int groupId);
+		Homeworks GetHomeworkById(int subjectId, int groupId);
 		void UpdateHomework(Homeworks homework);
 		void CreateNewHomework(Homeworks homework);
+		void CompleteHomeworkById(int homeworkId, int subjectId, int groupId);
 		void DeleteHomeworkById(int homeworkId, int subjectId, int groupId);
 	}
 }
