245d3f198b3268e43ec820a1fd1ce0881645042a
Heindale <heindale@yandex.ru>
2024-01-17 16:42:19 +0500

    add IExperienceService

---
+++ HomewOurK/Interfaces/IExperienceService.cs
@@ -0,0 +1,15 @@
+﻿using HomewOurK.Domain.Entities;
+
+namespace HomewOurK.Application.Interfaces
+{
+	public interface IExperienceService
+	{
+		void RaiseLevel(int userId, int groupId);
+		void IncrementCompletedHomeworksCount(int userId, int groupId);
+		void IncrementCreatedHomeworksCount(int userId, int groupId);
+		void AddExperience(int userId, int groupId, int experienceValue);
+		GroupsUsers GetInfoAboutExperience(int userId, int groupId);
+		List<GroupsUsers> GetInfoAboutExperienceByGroupId(int groupId);
+		List<GroupsUsers> GetInfoAboutExperienceByUserId(int userId);
+	}
+}
