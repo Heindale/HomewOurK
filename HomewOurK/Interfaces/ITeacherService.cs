95c4065d6cedcccb1575f398615db1fee3e15412
Heindale <heindale@yandex.ru>
2024-01-19 20:49:37 +0500

    update ITeacherService and IUserService

--- HomewOurK/Interfaces/ITeacherService.cs
+++ HomewOurK/Interfaces/ITeacherService.cs
@@ -10,8 +10,12 @@ namespace HomewOurK.Application.Interfaces
         bool UpdateTeacher(Teacher teacher);
         bool DeleteTeacher(Teacher teacher);
 
+        bool AddSubject(Teacher teacher, Subject subject);
         bool AddSubject(int teacherId, int groupId, int subjectId);
-        bool DeleteSubjectById(int teacherId, int groupId, int subjectId);
+        bool DeleteSubject(Teacher teacher, Subject subject);
+        bool DeleteSubject(int teacherId, int groupId, int subjectId);
+
         IEnumerable<Subject> GetSubjectsByTeacherId(int teacherId, int groupId);
+        IEnumerable<Subject> GetSubjectsFromTeacher(Teacher teacher);
     }
 }
--- HomewOurK/Interfaces/IUserService.cs
+++ HomewOurK/Interfaces/IUserService.cs
@@ -6,13 +6,13 @@ namespace HomewOurK.Application.Interfaces
 	{
 		User GetUserById(int userId);
 		IEnumerable<User> GetUsersByGroupId(int groupId);
-		bool RegisterNewUser(User user); // return true if user was registered
+		bool AddUser(User user); // return true if user was added
 		bool UpdateUser(User user); // return true if user was updated
 		bool DeleteUser(User user); // return true if user was deleted
 
-		bool AddUserToGroup(User user, System.Text.RegularExpressions.Group group); // return true if user was added to group
+		bool AddUserToGroup(User user, Group group); // return true if user was added to group
 		bool AddUserToGroup(int userId, int groupId); // return true if user was added to group
-		bool RemoveUserFromGroup(User user, System.Text.RegularExpressions.Group group); // return true if user was deleted from group
+		bool RemoveUserFromGroup(User user, Group group); // return true if user was deleted from group
 		bool RemoveUserFromGroup(int userId, int groupId); // return true if user was deleted from group
 	}
 }
