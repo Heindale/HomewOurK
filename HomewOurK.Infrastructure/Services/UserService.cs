16aaa4a553bd2daa6a2d449b2db6b224339d6db1
Heindale <heindale@yandex.ru>
2024-02-06 12:18:59 +0500

    add UserService

---
+++ HomewOurK.Infrastructure/Services/UserService.cs
@@ -0,0 +1,86 @@
+﻿using HomewOurK.Application.Interfaces;
+using HomewOurK.Application.Interfaces.Repositories;
+using HomewOurK.Domain.Entities;
+
+namespace HomewOurK.Infrastructure.Services
+{
+	public class UserService(IBaseEntityRepository<User> usersRepository, IGroupsUsersRepository groupsUsersRepository) : IUserService
+	{
+		private readonly IGroupsUsersRepository _groupsUsersRepository = groupsUsersRepository;
+		private readonly IBaseEntityRepository<User> _usersRepository = usersRepository;
+
+		public bool AddUser(User user)
+		{
+			return _usersRepository.Add(user);
+		}
+
+		public bool DeleteUser(User user)
+		{
+			return _usersRepository.Delete(user);
+		}
+
+		public User? GetUserById(int userId)
+		{
+			return _usersRepository.GetById(userId);
+		}
+
+		public IEnumerable<User> GetAll()
+		{
+			return _usersRepository.GetAll();
+		}
+
+		public bool UpdateUser(User user)
+		{
+			return _usersRepository.Update(user);
+		}
+
+		public bool AddUserToGroup(User user, Group group)
+		{
+			var groupsUsers = new GroupsUsers
+			{
+				Group = group,
+				GroupId = group.Id,
+				User = user,
+				UserId = user.Id
+			};
+
+			return _groupsUsersRepository.Add(groupsUsers);
+		}
+
+		public bool AddUserToGroup(int userId, int groupId)
+		{
+			return _groupsUsersRepository.Add(new GroupsUsers { GroupId = groupId, UserId = userId });
+		}
+
+		public IEnumerable<User> GetUsersByGroupId(int groupId)
+		{
+			var groupsUsers = _groupsUsersRepository.Entities.Where(x => x.GroupId == groupId).ToList();
+			List<User> users = [];
+			for (int i = 0; i < groupsUsers.Count; i++)
+			{
+				var newUser = _usersRepository.GetById(groupsUsers[i].UserId);
+				if (newUser is not null)
+					users.Add(newUser);
+			}
+			return users;
+		}
+
+		public bool RemoveUserFromGroup(User user, Group group)
+		{
+			var groupsUsers = new GroupsUsers
+			{
+				Group = group,
+				GroupId = group.Id,
+				User = user,
+				UserId = user.Id
+			};
+
+			return _groupsUsersRepository.Delete(groupsUsers);
+		}
+
+		public bool RemoveUserFromGroup(int userId, int groupId)
+		{
+			return _groupsUsersRepository.Delete(new GroupsUsers { GroupId = groupId, UserId = userId });
+		}
+	}
+}
--- HomewOurK/Interfaces/IUserService.cs
+++ HomewOurK/Interfaces/IUserService.cs
@@ -4,15 +4,24 @@ namespace HomewOurK.Application.Interfaces
 {
 	public interface IUserService
 	{
-		User GetUserById(int userId);
+		User? GetUserById(int userId);
+
 		IEnumerable<User> GetUsersByGroupId(int groupId);
+
+		IEnumerable<User> GetAll();
+
 		bool AddUser(User user); // return true if user was added
+
 		bool UpdateUser(User user); // return true if user was updated
+
 		bool DeleteUser(User user); // return true if user was deleted
 
 		bool AddUserToGroup(User user, Group group); // return true if user was added to group
+
 		bool AddUserToGroup(int userId, int groupId); // return true if user was added to group
+
 		bool RemoveUserFromGroup(User user, Group group); // return true if user was deleted from group
+
 		bool RemoveUserFromGroup(int userId, int groupId); // return true if user was deleted from group
 	}
 }
