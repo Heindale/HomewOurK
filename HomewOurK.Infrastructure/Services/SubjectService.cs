93112dd8e3bd4f3cf76276009ba804f625896ca1
Heindale <heindale@yandex.ru>
2024-02-18 23:57:02 +0500

    add SubjectService

---
+++ HomewOurK.Infrastructure/Services/SubjectService.cs
@@ -0,0 +1,100 @@
+﻿using HomewOurK.Application.Interfaces;
+using HomewOurK.Application.Interfaces.Repositories;
+using HomewOurK.Domain.Entities;
+
+namespace HomewOurK.Infrastructure.Services
+{
+	internal class SubjectService(IGroupElementRepository<Teacher> teacherRepository,
+			IGroupElementRepository<Subject> subjectRepository) : ISubjectService
+	{
+		private readonly IGroupElementRepository<Teacher> _teacherRepository = teacherRepository;
+		private readonly IGroupElementRepository<Subject> _subjectsRepository = subjectRepository;
+
+		public bool AddSubject(Subject subject)
+		{
+			return _subjectsRepository.Add(subject);
+		}
+
+		public bool AddTeacher(Subject subject, Teacher teacher)
+		{
+			if (teacher != null && subject != null)
+			{
+				subject.Teachers.Add(teacher);
+				return _subjectsRepository.Update(subject);
+			}
+			return false;
+		}
+
+		public bool AddTeacher(int subjectId, int groupId, int teacherId)
+		{
+			var teacher = _teacherRepository.GetById(teacherId, groupId);
+			var subject = _subjectsRepository.GetById(subjectId, groupId);
+
+			if (teacher != null && subject != null)
+			{
+				subject.Teachers.Add(teacher);
+				return _subjectsRepository.Update(subject);
+			}
+			return false;
+		}
+
+		public bool UpdateSubject(Subject subject)
+		{
+			return _subjectsRepository.Update(subject);
+		}
+
+		public bool DeleteSubject(Subject subject)
+		{
+			return _subjectsRepository.Delete(subject);
+		}
+
+		public Subject? GetSubjectById(int subjectId, int groupId)
+		{
+			return _subjectsRepository.GetById(subjectId, groupId);
+		}
+
+		public IEnumerable<Subject> GetSubjectsByGroupId(int groupId)
+		{
+			return _subjectsRepository.Entities.Where(x => x.GroupId == groupId).ToList();
+		}
+
+		public IEnumerable<Teacher>? GetTeachersBySubjectId(int subjectId, int groupId)
+		{
+			return _subjectsRepository.Entities
+				.Where(x => x.Id == subjectId && x.GroupId == groupId)
+				.Select(x => x.Teachers)
+				.FirstOrDefault();
+		}
+
+		public IEnumerable<Teacher>? GetTeachersFromSubject(Subject subject)
+		{
+			return _subjectsRepository.Entities
+				.Where(x => x.Id == subject.Id && x.GroupId == subject.GroupId)
+				.Select(x => x.Teachers)
+				.FirstOrDefault();
+		}
+
+		public bool DeleteTeacher(Subject subject, Teacher teacher)
+		{
+			if (teacher != null && subject != null)
+			{
+				subject.Teachers.Remove(teacher);
+				return _subjectsRepository.Update(subject);
+			}
+			return false;
+		}
+
+		public bool DeleteTeacher(int subjectId, int groupId, int teacherId)
+		{
+			var teacher = _teacherRepository.GetById(teacherId, groupId);
+			var subject = _subjectsRepository.GetById(subjectId, groupId);
+
+			if (teacher != null && subject != null)
+			{
+				subject.Teachers.Remove(teacher);
+				return _subjectsRepository.Update(subject);
+			}
+			return false;
+		}
+	}
+}
--- HomewOurK/Interfaces/ISubjectService.cs
+++ HomewOurK/Interfaces/ISubjectService.cs
@@ -4,18 +4,26 @@ namespace HomewOurK.Application.Interfaces
 {
 	public interface ISubjectService
 	{
-		Subject GetSubjectById(int subjectId, int groupId);
+		Subject? GetSubjectById(int subjectId, int groupId);
+
 		IEnumerable<Subject> GetSubjectsByGroupId(int groupId);
+
 		bool AddSubject(Subject subject);
+
 		bool UpdateSubject(Subject subject);
+
 		bool DeleteSubject(Subject subject);
 
 		bool AddTeacher(Subject subject, Teacher teacher);
-		bool AddTeacher(int subjectId, int teacherId);
-		bool DeleteTeacher(Subject subject, Teacher teacher);
-		bool DeleteTeacher(int subjectId, int teacherId);
 
-		IEnumerable<Teacher> GetTeachersBySubjectId(int subjectId, int groupId);
-		IEnumerable<Teacher> GetTeachersFromSubject(int subjectId, int groupId);
+		bool AddTeacher(int subjectId, int groupId, int teacherId);
+
+		bool DeleteTeacher(Subject subject, Teacher teacher);
+
+		bool DeleteTeacher(int subjectId, int groupId, int teacherId);
+
+		IEnumerable<Teacher>? GetTeachersBySubjectId(int subjectId, int groupId);
+
+		IEnumerable<Teacher>? GetTeachersFromSubject(Subject subject);
 	}
 }
