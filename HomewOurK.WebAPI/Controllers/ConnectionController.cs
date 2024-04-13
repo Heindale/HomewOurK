9654045a101233d6f5eaef353b318dac3dbf04a8
Heindale <heindale@yandex.ru>
2024-04-13 22:14:31 +0500

    add ConnectionController

---
+++ HomewOurK.WebAPI/Controllers/ConnectionController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HomewOurK.WebAPI.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ConnectionController : ControllerBase
+	{
+		[HttpGet]
+		public IActionResult GetStatus()
+		{
+			return Ok();
+		}
+	}
+}
